786d0da853dcd716eb4995fe219edc5a5de0cc9a
crowesn <crowesn@gmail.com>
2019-07-16 15:45:03 -0400

    customize journal form

--- app/forms/hyrax/journal_form.rb
+++ app/forms/hyrax/journal_form.rb
@@ -4,6 +4,16 @@ module Hyrax
   # Generated form for Journal
   class JournalForm < Hyrax::Forms::WorkForm
     self.model_class = ::Journal
-    self.terms += [:resource_type]
+    self.terms += %i[alternate_title extent frequency editor holding_institution geographic_location geo_subject]
+    ## remove fields
+    self.terms -= %i[identifier resource_type based_near creator contributor source keyword]
+
+    ## Setting custom required fields
+    self.required_fields = %i[title alternate_title extent date_created description editor publisher holding_institution geographic_location]
+
+  def primary_terms
+    %i[related_url title alternate_title extent frequency date_created description editor publisher holding_institution geographic_location subject geo_subject rights_statement license]
+    end
+
   end
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -8,6 +8,34 @@ class Journal < ActiveFedora::Base
   # self.valid_child_concerns = []
   validates :title, presence: { message: 'Your work must have a title.' }
 
+  property :alternate_title, predicate: ::RDF::URI.new('http://purl.org/dc/terms/title#alternative') do |index|
+    index.as :stored_searchable
+  end
+
+  property :frequency, predicate: ::RDF::URI.new('http://purl.org/dc/terms/description#frequncy'), multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :extent, predicate: ::RDF::URI.new('http://purl.org/dc/terms/extent') do |index|
+    index.as :stored_searchable
+  end
+
+  property :editor, predicate: ::RDF::URI.new('http://purl.org/dc/terms/contributor') do |index|
+    index.as :stored_searchable
+  end
+
+  property :holding_institution, predicate: ::RDF::URI.new('http://purl.org/dc/terms/provenance#holdingInstitution'), multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :geographic_location, predicate: ::RDF::URI.new('http://purl.org/dc/terms/spatial'), multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :geo_subject, predicate: ::RDF::URI.new('http://purl.org/dc/terms/coverage#spatial') do |index|
+    index.as :stored_searchable
+  end
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
